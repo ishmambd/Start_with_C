9cf50810bfcc7d771bf603128aa31ab4bbffdf9f
MD. ISHMAM JINAN <111278911+ishmambd@users.noreply.github.com>
2025-11-23 23:08:21 +0600

    Add files via upload

---
+++ favourite_num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int myfavouritenumber=0;
+    
+    printf("Please enter your favourite number\n");
+    scanf("%d",&myfavouritenumber);
+    printf("You entered number:%d\n",myfavouritenumber);
+
+    return 0;
+}
