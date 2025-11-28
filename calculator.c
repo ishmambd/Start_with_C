733d2745c1453b302082b9bc3ca32231ee046443
MD. ISHMAM JINAN <111278911+ishmambd@users.noreply.github.com>
2025-11-29 00:33:44 +0600

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);   
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Error: Division by zero is not allowed.\n");
+            } else {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            }
+            break;
+
+        default:
+            printf("Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
